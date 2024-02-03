b40cfa1b85404d897e80d804756d23e0929adaaa
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-02-03 18:16:33 -0500

    feat: implement nested route & applayout

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,33 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/orders/CreateOrder";
 import Order from "./features/orders/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "order/orderId",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "order/orderId",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 

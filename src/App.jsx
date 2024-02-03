61c67c3a0274d954b1a4c093351f45519aa35dd1
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-02-03 17:56:57 -0500

    feat: implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
+// components
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/orders/CreateOrder";
+import Order from "./features/orders/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "order/orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <>Helo</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

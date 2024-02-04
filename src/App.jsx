2d2a16e1f14ef63d7f01da109361153c78f70334
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-02-03 20:00:47 -0500

    feat: add Form from react router to post the form data

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import "./App.css";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/orders/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/orders/CreateOrder";
 import Order, { loader as orderLoader } from "./features/orders/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
@@ -33,6 +35,7 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
